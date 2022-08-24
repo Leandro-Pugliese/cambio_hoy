b47f020fa8af48fe8423b1a6cb7c1a515d2e25ec
Leandro-Pugliese <lean.pugliese@gmail.com>
2022-08-24 20:05:36 -0300

    Cambio_Hoy-V1.0-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const host = "https://api.frankfurter.app"
+  const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+
+  const [oficial, setOficial] = useState("");
+  const [blue, setBlue] = useState("");
+  const [tarjeta, setTarjeta] = useState("");
+  const [bolsa, setBolsa] = useState("");
+  const [liqui, setLiqui] = useState("");
+
+  const buscar = async () => {
+
+    let dolar = await fetch(url);
+    let dataDolar = await dolar.json();
+
+    let oficialDolar = `${dataDolar[0].casa.nombre} =
+                        Compra: $${dataDolar[0].casa.compra} /
+                        Venta: $${dataDolar[0].casa.venta}`
+
+    let blueDolar = `${dataDolar[1].casa.nombre} =
+                      Compra: $${dataDolar[1].casa.compra} /
+                      Venta: $${dataDolar[1].casa.venta}`
+    
+    let tarjetaDolar = `${dataDolar[6].casa.nombre} (tarjeta) =
+                        Compra: $${dataDolar[6].casa.compra} /
+                        Venta: $${dataDolar[6].casa.venta}`
+                 
+    let bolsaDolar = `${dataDolar[4].casa.nombre} =
+                        Compra: $${dataDolar[4].casa.compra} /
+                        Venta: $${dataDolar[4].casa.venta}`
+    
+    let liquiDolar = `${dataDolar[3].casa.nombre} =
+                        Compra: $${dataDolar[3].casa.compra} /
+                        Venta: $${dataDolar[3].casa.venta}`
+    
+    setOficial(oficialDolar);
+    setBlue(blueDolar);
+    setTarjeta(tarjetaDolar);
+    setBolsa(bolsaDolar);
+    setLiqui(liquiDolar);
+
+  }
+
+  const [gbp, setGbp] = useState("");
+  const [euro, setEuro] = useState("");
+  const [jpy, setJpy] = useState("");
+  const [text, setTexto] = useState("");
+  const buscarOtros = async () => {
+    
+    const texto = `Otras cotizaciones`
+    setTexto(texto)
+
+    let gbpDolar = await axios(`${host}/latest?amount=1&from=GBP&to=USD`);
+    let gbpData = await gbpDolar.data
+    let pound = `GBP (Libra esterlina) = ${gbpData.rates.USD} USD (${gbpData.date})` 
+    setGbp(pound);
+    
+    let euroDolar = await axios(`${host}/latest?amount=1&from=EUR&to=USD`);
+    let euroData = await euroDolar.data
+    let eur = `EURO = ${euroData.rates.USD} USD (${euroData.date})` 
+    setEuro(eur);
+
+    let jpyDolar = await axios(`${host}/latest?amount=1&from=USD&to=JPY`);
+    let jpyData = await jpyDolar.data
+    let japan = `USD = ${jpyData.rates.JPY} JPY (Yen) (${jpyData.date})` 
+    setJpy(japan);
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='titulo'>
+        <h2>Cambio Hoy</h2>
+        <p><b>¿qué cotización estas buscando?</b></p>
+      </div>
+      <div className="botones">
+        <div>
+          <button type="button" id="boton" onClick={buscar}>Dolar - ARS</button>
+        </div>
+        <div>
+          <button type="button" id="boton2" onClick={buscarOtros}>Dolar - Otros</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div className="dolar" id="dolar">
+            <p>{oficial}</p>
+          </div>
+          <div className="dolar" id="blue">
+            <p>{blue}</p>
+          </div>
+          <div className="dolar" id="tarjeta">
+            <p>{tarjeta}</p>
+          </div>
+          <div className="dolar" id="bolsa">
+            <p>{bolsa}</p>
+          </div>
+          <div className="dolar" id="liqui">
+            <p>{liqui}</p>
+          </div>
+        </div>
+        <div id="pares">
+          <div id="otros">
+            <h4>{text}</h4>
+          </div>
+          <div id="GBP-USD">
+            <p>{gbp}</p>
+          </div>
+          <div id="EUR-USD">
+            <p>{euro}</p>
+          </div>
+          <div id="USD-JPY">
+            <p>{jpy}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
