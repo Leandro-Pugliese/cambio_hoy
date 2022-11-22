ddd44ab89f5f122c82e2647ce13f9d4ba5e7ff29
Leandro-Pugliese <lean.pugliese@gmail.com>
2022-11-22 11:21:40 -0300

    Cambio_Hoy-V2.0(CryptoCurrenciesAdded)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-
+  //Rutas generales
   const host = "https://api.frankfurter.app"
   const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+  const coinBase = "https://api.coinbase.com/v2"
 
+  //DolarSi Api
   const [oficial, setOficial] = useState("");
   const [blue, setBlue] = useState("");
   const [tarjeta, setTarjeta] = useState("");
@@ -47,10 +49,12 @@ function App() {
 
   }
 
+  //FrankFurter Api
   const [gbp, setGbp] = useState("");
   const [euro, setEuro] = useState("");
   const [jpy, setJpy] = useState("");
   const [text, setTexto] = useState("");
+  
   const buscarOtros = async () => {
     
     const texto = `Otras cotizaciones`
@@ -70,9 +74,54 @@ function App() {
     let jpyData = await jpyDolar.data
     let japan = `USD = ${jpyData.rates.JPY} JPY (Yen) (${jpyData.date})` 
     setJpy(japan);
-
-
   }
+  
+  //CoinBase Api
+  const [ethereum, setEth] = useState("");
+  const [bitcoin, setBtc] = useState("");
+  const [cardano, setAda] = useState("");
+  const [solana, setSol] = useState("");
+  const [tether, setUsdt] = useState("");
+  const [polygon, setMatic] = useState("");
+  const [crpytoText, setCryptoText] = useState("");
+
+  const crypto = async () => {
+
+    const textoCrypto =  `Cryptomonedas`
+    setCryptoText(textoCrypto)
+
+    let btc_USD = await axios(`${coinBase}/prices/BTC-USD/buy`)
+    let btc_USD_response = await btc_USD.data
+    let btc = `BTC (Bitcoin) = $${btc_USD_response.data.amount} USD`
+    setBtc(btc)
+
+    let eth_USD = await axios(`${coinBase}/prices/ETH-USD/buy`)
+    let eth_USD_response = await eth_USD.data
+    let eth = `BTC (Ethereum) = $${eth_USD_response.data.amount} USD`
+    setEth(eth)
+
+    let ada_USD = await axios(`${coinBase}/prices/ADA-USD/buy`)
+    let ada_USD_response = await ada_USD.data
+    let ada = `ADA (Cardano) = $${ada_USD_response.data.amount} USD`
+    setAda(ada)
+
+    let sol_USD = await axios(`${coinBase}/prices/SOL-USD/buy`)
+    let sol_USD_response = await sol_USD.data
+    let sol = `SOL (Solana) = $${sol_USD_response.data.amount} USD`
+    setSol(sol)
+
+    let usdt_usd = await axios(`${coinBase}/prices/USDT-USD/buy`)
+    let usdt_usd_response = await usdt_usd.data
+    let usdt = `USDT (Tether)= $${usdt_usd_response.data.amount} USD`
+    setUsdt(usdt)
+
+    let matic_usd = await axios(`${coinBase}/prices/MATIC-USD/buy`)
+    let matic_usd_response = await matic_usd.data
+    let matic = `MATIC (Polygon)= $${matic_usd_response.data.amount} USD`
+    setMatic(matic)
+  }
+    
+  
 
   return (
     <div className="App">
@@ -86,6 +135,9 @@ function App() {
         </div>
         <div>
           <button type="button" id="boton2" onClick={buscarOtros}>Dolar - Otros</button>
+        </div>
+        <div>
+          <button type="button" id="boton2" onClick={crypto}>Crypto</button>
         </div>
       </div>
       <div>
@@ -118,6 +170,29 @@ function App() {
           </div>
           <div id="USD-JPY">
             <p>{jpy}</p>
+          </div>
+        </div>
+        <div id="cryptos">
+          <div id="cryptomonedas">
+            <h4>{crpytoText}</h4>
+          </div>
+          <div id="USDT">
+            <p>{tether}</p>
+          </div>
+          <div id="BTC">
+            <h4>{bitcoin}</h4>
+          </div>
+          <div id="ETH">
+            <h4>{ethereum}</h4>
+          </div>
+          <div id="ADA">
+            <p>{cardano}</p>
+          </div>
+          <div id="SOL">
+            <p>{solana}</p>
+          </div>
+          <div id="MATIC">
+            <p>{polygon}</p>
           </div>
         </div>
       </div>
